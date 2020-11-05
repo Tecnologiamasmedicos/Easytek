2bd6a9c5c5a1f15ebb7f77b729501fb34ec9bfa4
Daniel Arevalo <dr@todoo.co>
2020-11-05 08:50:47 -0500

    Correccion logger

--- web_sale_extended/models/models.py
+++ web_sale_extended/models/models.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 import logging
-logger = logging.getLogger(name_)
+_logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
