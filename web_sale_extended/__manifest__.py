7b9aceb104d6c99042e7902e500a1e90bf6edb61
Daniel Arevalo <dr@todoo.co>
2020-11-05 08:29:24 -0500

    Se añadio vista beneficiario

--- web_sale_extended/__manifest__.py
+++ web_sale_extended/__manifest__.py
@@ -27,7 +27,8 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/web_sale_shop_address.xml'
+        'views/web_sale_shop_address.xml',
+        'views/beneficiary.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
