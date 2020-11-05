21879917f23d522a05a997aa262e2cd987a05337
Daniel Arevalo <dr@todoo.co>
2020-11-05 08:29:24 -0500

    Se añadio vista beneficiario

--- web_sale_extended/models/models.py
+++ web_sale_extended/models/models.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+import logging
+logger = logging.getLogger(name_)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    website_partner_type = fields.Char(string='partner_type', compute='_get_website_partner_type', store=False)
+    
+    
+    @api.depends('zip','city_id')
+    def _get_website_partner_type(self):
+        for record in self:
+            record.website_partner_type = record.zip + record.street
 
 
 # class web_sale_extended(models.Model):
