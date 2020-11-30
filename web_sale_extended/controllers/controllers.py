b3a853f96e82e5892037eda0a18d586f46e900d6
Daniel Arevalo <dr@todoo.co>
2020-11-30 12:32:00 -0500

    reporte

--- web_sale_extended/controllers/controllers.py
+++ web_sale_extended/controllers/controllers.py
@@ -342,7 +342,7 @@ class WebsiteSaleExtended(WebsiteSale):
     def report_poliza(self, city_id=None, **kwargs):
         
         report_obj = request.env['ir.actions.report']
-        report = report_obj._get_report_from_name('web_sale_extended.report_customreport_customeasytek_template_res_partner')
+        report = report_obj._get_report_from_name('web_sale_extended.report_customreport_customeasytek_template')
         _logger.info("reporte***************************************************")
         _logger.info(report[0])
         pdf = report.render_qweb_pdf()
