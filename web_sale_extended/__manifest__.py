41a9fbd80e7208ed14b0205f8478a3068cc1ad2e
FelipeNavasTodoo <ln@todoo.co>
2021-08-30 14:39:59 -0500

    [FIX] manifest

--- web_sale_extended/__manifest__.py
+++ web_sale_extended/__manifest__.py
@@ -45,7 +45,7 @@
         'data/tus_datos_bot.xml',
         'data/payu_latam_bot.xml',
         'data/payment_invoice_bot.xml',
-        'data/collection_file_bot.xml'
+        'data/collection_file_bot.xml',
         'data/beneficiary_confirm_template.xml',
         'views/res_users_view.xml'
     ],
