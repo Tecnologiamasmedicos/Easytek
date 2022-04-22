366a8259a50090177752568da50b7559471c7bd6
dv1-masmedicos <dv1@masmedicos.co>
2022-04-22 10:51:13 -0500

    [ADD] campos al reporte de liquidaciones

--- web_sale_extended/reports/report_settlement.py
+++ web_sale_extended/reports/report_settlement.py
@@ -21,6 +21,9 @@ class ReportSubscriptionSettlement(models.Model):
     subscription_date_start = fields.Date('Fecha de Inicio',readonly=True)
     subscription_date_end = fields.Date('Fecha Final',readonly=True)
     invoice_date = fields.Date('Fecha Liquidación',readonly=True)
+    product_code = fields.Char(string='Código producto', readonly=True)
+    value = fields.Float('Valor', readonly=True)
+    sponsor_id = fields.Many2one('res.partner', string="Sponsor", store=True)
     payulatam_order_id = fields.Char('Order ID PayU', readonly=True)
     payulatam_state = fields.Selection([
         ("APPROVED", "APROBADO"), 
@@ -35,10 +38,8 @@ class ReportSubscriptionSettlement(models.Model):
         tools.drop_view_if_exists(self._cr, 'report_subscription_settlement')
         query = """
         CREATE or REPLACE VIEW report_subscription_settlement AS(
-        
         select 
         row_number() OVER (ORDER BY sub.id) as id,
-        
         sorder.id as sale_order,
         sub.id as subscription,
         inv.id as invoice,
@@ -47,15 +48,20 @@ class ReportSubscriptionSettlement(models.Model):
         sub.date_start as subscription_date_start,
         sub.date as subscription_date_end,
         inv.invoice_date as invoice_date,
+        pro.default_code as product_code,
+        inv.amount_total as value,
+        inv.sponsor_id as sponsor_id,
         inv.payulatam_order_id as payulatam_order_id,
         inv.payulatam_state as payulatam_state,
         inv.payulatam_datetime as payulatam_datetime,
         p.name as ap_name
-        
+
         from account_move inv
         left join sale_subscription sub on sub.code = inv.invoice_origin
         left join sale_order sorder on sorder.subscription_id = sub.id
         left join res_partner p on p.id = inv.partner_id
+        left join account_move_line line on line.move_id = inv.id
+        left join product_product pro on pro.id = line.product_id
         
         where 1=1 and inv.state='finalized'
 	and inv.invoice_origin is not null
